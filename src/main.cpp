bef8428e1016358ead8f120b3d54d09be4130c21
Farcas Adrian Tiberiu <farcasadriantiberiu@gmail.com>
2023-10-27 23:54:01 +0300

    early structure of the main class used

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
 # include <cstring>
-# include "class.hpp"
+# include "country.hpp"
 # include <iostream>
 
-# define MAX_LEN 5
-
 int main() {
-    char * s = new char[MAX_LEN];
-    memcpy(s, "Sal", MAX_LEN);
-    Class1 c1(1, 2.0, s);
-    {
-        Class1 y = c1;
-    }
-    Class1 c2 = std::move(Class1(2, 2.0, s));
-    return 0;
+
 }
