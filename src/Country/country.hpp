545a107b606a0e8c446605d6c77ea59708ef4006
Farcas Adrian Tiberiu <farcasadriantiberiu@gmail.com>
2023-10-28 00:18:00 +0300

    finish defining the country class

--- src/Country/country.hpp
+++ src/Country/country.hpp
@@ -2,4 +2,37 @@
 
 class Country {
 
+};#pragma once
+
+class Country {
+private:
+    std::string name;
+    int population;
+    float area;
+
+public:
+    // Constructor
+    Country(std::string nume, int varsta, float area);
+
+    // Destructor
+    Country();
+
+    //Copy Constructor
+    Country(const Country& prevCountry);
+
+    //Move Constructor
+    Country(Country&& prevCountry);
+
+    //Getters
+    std::string getName();
+    int getPopulation();
+    float getArea();
+
+    //Setters
+    void setName(std::string newName);
+    void setPopulation(int newPopulation);
+    void setArea(float newArea);
+
+    //Calculate Country Density
+    float calculateCountryDensity(int population, float area);
 };
